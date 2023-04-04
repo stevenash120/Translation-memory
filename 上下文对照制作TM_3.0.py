bdaf0d4f0fc83d557e21f4a7b18610bd58fa450b
stevenash120 <30543663+stevenash120@users.noreply.github.com>
2023-04-04 16:04:32 +0800

    Update 上下文对照制作TM_3.0.py

--- 上下文对照制作TM_3.0.py
+++ 上下文对照制作TM_3.0.py
@@ -16,26 +16,34 @@ ws = wb.active
 ws.cell(row=1, column=1).value = "zhCN"
 ws.cell(row=1, column=2).value = "enUS"
 
+# 从第二行开始逐行写入 Word 文档的内容
+row_index = 2
 for doc_file in doc_files:
     if not doc_file.startswith("~$"):
         # 打开 Word 文档
         doc = docx.Document(doc_file)
 
-        # 提取中英文对照内容并写入 Excel
-        for i in range(len(doc.paragraphs)):
-            if i % 2 == 0:
-                # 中文
-                ws.cell(row=i // 2 + 2, column=1).value = doc.paragraphs[i].text
-            else:
-                # 英文
-                ws.cell(row=i // 2 + 2, column=2).value = doc.paragraphs[i].text
+        # 判断第一段是否为一级标题，如果是则删除
+        if doc.paragraphs[0].style.name == 'Heading 1':
+            del doc.paragraphs[0]
+
+        # 逐行写入中英文对照内容
+        for p in range(len(doc.paragraphs)):
+            if doc.paragraphs[p].text.strip(): # 确保段落非空
+                if p % 2 == 0:
+                    # 中文
+                    ws.cell(row=row_index, column=1).value = doc.paragraphs[p].text
+                else:
+                    # 英文
+                    ws.cell(row=row_index, column=2).value = doc.paragraphs[p].text
+                    row_index += 1
 
 # 删除空白行
 for i in range(ws.max_row, 0, -1):
     if not ws.cell(row=i, column=1).value and not ws.cell(row=i, column=2).value:
         ws.delete_rows(i)
 
-# 在汇总Excel的首行第一列和第二列重新标注zhCN和enUS
+# 在汇总 Excel 的首行第一列和第二列重新标注 zhCN 和 enUS
 ws.cell(row=1, column=1).value = "zhCN"
 ws.cell(row=1, column=2).value = "enUS"
 
